1112f21cc1d3e1e0fbe18e11ff022cd780d2af12
liuyuan <805764579@qq.com>
2020-03-02 00:02:06 +0800

    Stream修改

--- src/main/java/com/ly/common/utils/StreamUtil.java
+++ src/main/java/com/ly/common/utils/StreamUtil.java
@@ -2,32 +2,39 @@ package com.ly.common.utils;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.InflaterInputStream;
 
 public class StreamUtil {
 
-	/*
-	 * public static List<String> read(InputStream InputStream){ return null;
-	 * 
-	 * }
-	 * 
-	 * public static List<String> read(String filePath){ return null;
-	 * 
-	 * }
-	 */
+	//读取InputStream放入集合
+	  public static List<String> read(InputStream inputStream) throws IOException{ 
+	  	List<String> list = new ArrayList<String>();
+	  	BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+	  	String line="";
+	  	while((line=reader.readLine())!=null) {
+	  		list.add(line);
+	  	}
+	  	return list;
+	  	
+	  }
+	  //根据文件的地址读取放入集合中
+	public static List<String> read(String filePath) throws IOException{ 
+	  	FileInputStream inputStream = new FileInputStream(filePath);
+	  	return read(inputStream);
+	  	
+	  }
+	 
 	//读取文件内容放入集合
 	public static List<String> read(File file) throws IOException{
-		List<String> list = new ArrayList<String>();
-		
-		BufferedReader reader = new BufferedReader(new FileReader(file));
-		String line="";
-		while ((line=reader.readLine())!=null) {
-			list.add(line);
-		}
-		
-		return list;
+		FileInputStream inputStream = new FileInputStream(file);
+		return read(inputStream);
 	}
 }
--- src/test/java/com/ly/common/utils/StreamUtilTest.java
+++ src/test/java/com/ly/common/utils/StreamUtilTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 public class StreamUtilTest {
 
 	@Test
-	public void test() throws IOException {
+	public void testRead() throws IOException {
 		List<String> list = StreamUtil.read(new File("d:\\data.text"));
 		for (String string : list) {
 			System.out.println(string);
